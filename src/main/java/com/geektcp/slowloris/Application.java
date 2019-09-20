4275f512288fe2800db6a929365a2f066b9b912d
nagle <geektcp@163.com>
2019-09-20 21:55:06 +0800

    build slowloris

--- src/main/java/com/geektcp/slowloris/Application.java
+++ src/main/java/com/geektcp/slowloris/Application.java
@@ -4,4 +4,27 @@ package com.geektcp.slowloris;
  * Created by TangHaiyang on 2019/9/19.
  */
 public class Application {
+    public static void main(String[] args) {
+        System.out.println("start slowloris!!!");
+        slowloris();
+    }
+
+    private static int slowloris() {
+        return 0;
+    }
+
+
+    private static int test_timeout(){
+        return 0;
+    }
+
+    private static Object str_split(){
+
+        return null;
+    }
+
+
+
+
 }
+
